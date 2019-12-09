85e218416c5e33797a6c2ba1aefece0785555c1d
Méloé <meloscavezzoni@gmail.com>
2019-12-09 07:43:15 +0100

    Loading

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Loading
+window.addEventListener("load", () => {
+  document.getElementById("mainCont").classList.remove("isLoading");
+});
+
+// Menu
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
